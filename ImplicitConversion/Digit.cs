e0bdfec113a1169626b995cd390005cef5694111
SvetoslavKadiev <svetoslav.kadiev@gmail.com>
2017-12-31 18:01:43 +0200

    Conversion Keywords
    https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/conversion-keywords

---
+++ ImplicitConversion/Digit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImplicitConversion
+{
+    class Digit
+    {
+        public Digit(double d)
+        {
+            val = d;
+        }
+
+        public double val;
+        // ...other members
+
+        // User-defined conversion from Digit to double
+        public static implicit operator double(Digit d)
+        {
+            return d.val;
+        }
+
+        //  User-defined conversion from double to Digit
+        public static implicit operator Digit(double d)
+        {
+            return new Digit(d);
+        }
+    }
+}
--- ImplicitConversion/Program.cs
+++ ImplicitConversion/Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace ImplicitConversion
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            Digit dig = new Digit(7);
+
+            //This call invokes the implicit "double" operator
+            double num = dig;
+
+            //This call invokes the implicit "Digit" operator
+            Digit dig2 = 12;
+
+            WriteLine("num = {0} dig2 = {1}", num, dig2.val);
+            ReadLine();
         }
     }
 }
