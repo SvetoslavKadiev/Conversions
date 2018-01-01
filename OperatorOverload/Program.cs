816613d9b26e4e9911f8565a1dba14e4220be4b7
SvetoslavKadiev <svetoslav.kadiev@gmail.com>
2018-01-01 22:56:44 +0200

    Add operator overload project

---
+++ OperatorOverload/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main()
+        {
+            Fraction a = new Fraction(1, 2);
+            Fraction b = new Fraction(3, 7);
+            Fraction c = new Fraction(2, 3);
+
+            WriteLine((double)(a * b + c));
+            ReadLine();
+        }
+    }
+}
